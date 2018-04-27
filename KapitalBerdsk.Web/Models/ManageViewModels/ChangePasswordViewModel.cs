91ca5ff1b8c9e6dd7844c5d2c00c56b3c14236d7
eshangin <shangin.jekon@mail.ru>
2018-04-27 19:12:37 +0700

    remove redundant logic

--- KapitalBerdsk.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
+++ KapitalBerdsk.Web/Models/ManageViewModels/ChangePasswordViewModel.cs
@@ -8,20 +8,20 @@ namespace KapitalBerdsk.Web.Models.ManageViewModels
 {
     public class ChangePasswordViewModel
     {
-        [Required]
+        [Required(ErrorMessage = Resources.ResourceKeys.Required)]
         [DataType(DataType.Password)]
-        [Display(Name = "Current password")]
+        [Display(Name = "Текущий пароль")]
         public string OldPassword { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = Resources.ResourceKeys.Required)]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "New password")]
+        [Display(Name = "Новый пароль")]
         public string NewPassword { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm new password")]
-        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match.")]
+        [Display(Name = "Повторите новый пароль")]
+        [Compare("NewPassword", ErrorMessage = "Новый пароль и его подтверждение не совпадают")]
         public string ConfirmPassword { get; set; }
 
         public string StatusMessage { get; set; }
