5c2c213b5cd052a21216672f62bbcb9cd3882f39
eshangin <shangin.jekon@mail.ru>
2018-05-05 16:14:49 +0700

    ApplicationUser DateCreated, DateUpdated

--- KapitalBerdsk.Web/Data/Migrations/20180505091209_ApplicationUser_DateCreated_DateUpdated.Designer.cs
+++ KapitalBerdsk.Web/Data/Migrations/20180505091209_ApplicationUser_DateCreated_DateUpdated.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace KapitalBerdsk.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180505091209_ApplicationUser_DateCreated_DateUpdated")]
+    partial class ApplicationUser_DateCreated_DateUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- KapitalBerdsk.Web/Models/ApplicationUser.cs
+++ KapitalBerdsk.Web/Models/ApplicationUser.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using KapitalBerdsk.Web.Data.Interfaces;
 using Microsoft.AspNetCore.Identity;
 
 namespace KapitalBerdsk.Web.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser, IAuditable
     {
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
     }
 }
