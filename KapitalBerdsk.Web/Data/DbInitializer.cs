2967e3710e396e870bcaa2a08629b68226c26e1d
eshangin <shangin.jekon@mail.ru>
2018-05-05 23:12:17 +0700

    Seed database: create default user

---
+++ KapitalBerdsk.Web/Data/DbInitializer.cs
@@ -0,0 +1,42 @@
+﻿using KapitalBerdsk.Web.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KapitalBerdsk.Web.Data
+{
+    public class DbInitializer
+    {
+        public async Task Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger logger)
+        {
+            if (context.Users.Any())
+            {
+                // DB has been seeded
+                return;
+            }
+
+            await CreateDefaultUser(userManager, logger);
+        }
+
+        private async Task CreateDefaultUser(UserManager<ApplicationUser> userManager, ILogger logger)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = "admin@admin.com",
+                Email = "admin@admin.com",                
+            };
+            const string defaultPwd = "abcdefg1";
+
+            var result = await userManager.CreateAsync(user, defaultPwd);
+            
+            if (!result.Succeeded)
+            {
+                var error = result.Errors.First();
+                logger.LogWarning($"Default user wasn't created. Error: {error.Code}, {error.Description}");
+            }
+        }
+    }
+}
--- KapitalBerdsk.Web/Program.cs
+++ KapitalBerdsk.Web/Program.cs
@@ -7,6 +7,10 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using KapitalBerdsk.Web.Data;
+using Microsoft.AspNetCore.Identity;
+using KapitalBerdsk.Web.Models;
 
 namespace KapitalBerdsk.Web
 {
@@ -14,7 +18,27 @@ namespace KapitalBerdsk.Web
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            var host = BuildWebHost(args);
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<ApplicationDbContext>();
+                var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                var dbInitializerLogger = services.GetRequiredService<ILogger<DbInitializer>>();
+
+                try
+                {
+                    new DbInitializer().Initialize(context, userManager, dbInitializerLogger).Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
