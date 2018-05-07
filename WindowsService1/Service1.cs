a7d1a3d80e7228f4836ac8c9182be4c73dc57b23
eshangin <shangin.jekon@mail.ru>
2018-05-07 22:43:21 +0700

    Test win service deployment

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,36 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private BackgroundJobServer _server;
+
+        public Service1()
+        {
+            InitializeComponent();
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(
+                System.Configuration.ConfigurationManager.ConnectionStrings[""].ConnectionString);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            _server = new BackgroundJobServer();
+        }
+
+        protected override void OnStop()
+        {
+            _server.Dispose();
+        }
+    }
+}
