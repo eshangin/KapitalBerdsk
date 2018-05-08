2f4460536ccba00d0d7b08b15c530527f3354b1f
eshangin <shangin.jekon@mail.ru>
2018-05-09 01:29:52 +0700

    Win service changes

---
+++ KapitalBerdsk.WindowsService/ReinstallService.bat
@@ -0,0 +1,3 @@
+%windir%\Microsoft.NET\Framework\v4.0.30319\installutil.exe /u %~dp0\bin\Debug\KapitalBerdsk.WindowsService.exe
+
+%windir%\Microsoft.NET\Framework\v4.0.30319\installutil.exe %~dp0\bin\Debug\KapitalBerdsk.WindowsService.exe
--- KapitalBerdsk.WindowsService/Service1.cs
+++ KapitalBerdsk.WindowsService/Service1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hangfire;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,33 @@ namespace KapitalBerdsk.WindowsService
 {
     public partial class Service1 : ServiceBase
     {
+        private BackgroundJobServer _server;
+
         public Service1()
         {
             InitializeComponent();
+
+            System.Threading.Thread.Sleep(10000);
+
+            GlobalConfiguration.Configuration.UseSqlServerStorage(
+                System.Configuration.ConfigurationManager.ConnectionStrings["Hangfire.ConnectionString"].ConnectionString);
         }
 
         protected override void OnStart(string[] args)
         {
+            _server = new BackgroundJobServer();
+
+            var timezone = TimeZoneInfo.CreateCustomTimeZone("nsk", new TimeSpan(07, 00, 00), "(GMT+07:00) Nsk", "nsk");
+
+            RecurringJob.AddOrUpdate("Check Building Object Closing Contracts", 
+                () => Console.WriteLine("Daily Job"),
+                "0 0 10 ? * MON-FRI *",
+                timezone);
         }
 
         protected override void OnStop()
         {
+            _server.Dispose();
         }
     }
 }
