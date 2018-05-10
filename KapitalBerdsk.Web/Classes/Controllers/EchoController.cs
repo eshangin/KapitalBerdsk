78fde57c4a030e7192ad5c67fad05229a2c1d9c4
eshangin <shangin.jekon@mail.ru>
2018-05-10 18:03:45 +0700

    Echo API

---
+++ KapitalBerdsk.Web/Classes/Controllers/EchoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KapitalBerdsk.Web.Classes.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Echo")]
+    public class EchoController : Controller
+    {
+        // GET: api/Echo
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
