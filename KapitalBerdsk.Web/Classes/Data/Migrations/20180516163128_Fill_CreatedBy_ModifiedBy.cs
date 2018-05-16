09d3894039b3b09adafb4a5ff22fc8aa45e04137
eshangin <shangin.jekon@mail.ru>
2018-05-16 23:40:35 +0700

    Fill CreatedBy and ModifiedBy for all auditable

---
+++ KapitalBerdsk.Web/Classes/Data/Migrations/20180516163128_Fill_CreatedBy_ModifiedBy.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KapitalBerdsk.Web.Classes.Data.Migrations
+{
+    public partial class Fill_CreatedBy_ModifiedBy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+declare @admin nvarchar(450)
+
+select top (1) @admin = u.Id from 
+dbo.AspNetUsers u 
+order by u.datecreated
+
+update dbo.AspNetUsers set CreatedById = @admin, ModifiedById = @admin
+
+declare @firstManager nvarchar(450)
+
+select top (1) @firstManager = u.Id from 
+dbo.AspNetUsers u 
+inner join dbo.AspNetUserRoles ur on u.Id = ur.UserId
+inner join dbo.AspNetRoles r on ur.RoleId = r.Id
+where r.Name = 'Manager'
+order by u.datecreated
+
+update dbo.BuildingObjects set CreatedById = @firstManager, ModifiedById = @firstManager
+update dbo.Employees set CreatedById = @firstManager, ModifiedById = @firstManager
+update dbo.FundsFlows set CreatedById = @firstManager, ModifiedById = @firstManager
+update dbo.Organizations set CreatedById = @firstManager, ModifiedById = @firstManager
+update dbo.PdSections set CreatedById = @firstManager, ModifiedById = @firstManager
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
