7dea17b4d4f5e465a5de9518175da3a9d994dce8
eshangin <shangin.jekon@mail.ru>
2018-05-18 22:13:05 +0700

    Convert Salary BuildingObject to FundsFlow with Salary OutgoType

--- KapitalBerdsk.Web/Classes/Data/Migrations/20180518150312_Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType.Designer.cs
+++ KapitalBerdsk.Web/Classes/Data/Migrations/20180518150312_Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace KapitalBerdsk.Web.Classes.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180518150312_Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType")]
+    partial class Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ KapitalBerdsk.Web/Classes/Data/Migrations/20180518150312_Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace KapitalBerdsk.Web.Classes.Data.Migrations
+{
+    public partial class Convert_SalaryBuildingObject_To_Row_With_Salary_OutgoType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+with ff (Id, BuildingObjectId, OutgoType)
+AS
+(
+	select ff.Id, ff.BuildingObjectId, ff.OutgoType
+	from 
+		dbo.FundsFlows ff
+		inner join dbo.BuildingObjects bo on ff.BuildingObjectId = bo.Id
+	where
+		bo.Name = N'оклад'
+)
+update ff set BuildingObjectId = NULL, OutgoType = 4
+
+delete from dbo.BuildingObjects where Name = N'оклад'
+");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
